69b700b14e1206cc67ab3c86f6d388abaa7e4e16
Shivam Punia <42895980+workwithshivi@users.noreply.github.com>
2025-01-12 15:44:56 +0530

    Initial Commit

---
+++ EndUserMS/src/main/java/com/app/flavourful/EndUserMS/EndUserMsApplication.java
@@ -0,0 +1,13 @@
+package com.app.flavourful.EndUserMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndUserMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndUserMsApplication.class, args);
+	}
+
+}
---
+++ EndUserMS/src/test/java/com/app/flavourful/EndUserMS/EndUserMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.flavourful.EndUserMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndUserMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
